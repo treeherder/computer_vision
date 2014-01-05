27e6b3bcdbf9f7fb71bd0aa6be2e629eb6680ca2
treeherder <b.d.reddybest@gmail.com>
2014-01-05 00:49:01 -0800

    thresholding levels for cell
    solar cell is clearly distinguishable from everything else on the
    plane.  time to work out bounding, and translate that to the cartesian
    coordinate  system

--- src/thresholding.py
+++ src/thresholding.py
@@ -5,12 +5,16 @@ def threshold(z):
   img = cv2.imread("../images/{0}".format(z))
   hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
   (hue, sat, val)  = cv2.split(hsv)
-  hf, hue  = cv2.threshold(hue, 100, 127, cv2.THRESH_BINARY)
-  sf, sat  = cv2.threshold(sat, 100, 127, cv2.THRESH_BINARY)
-  vf, val  = cv2.threshold(val, 120, 157, cv2.THRESH_BINARY)
+  hf, hue  = cv2.threshold(hue, 0, 20, cv2.THRESH_BINARY)
+  sf, sat  = cv2.threshold(sat, 0, 20, cv2.THRESH_BINARY)
+  vf, val  = cv2.threshold(val, 30, 45, cv2.THRESH_BINARY)
   mrgd = cv2.merge([hue, sat, val])
   bgr = cv2.cvtColor(mrgd, cv2.COLOR_HSV2BGR)
   gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
   cv2.imwrite("../images/t-{0}".format(z), gray)
-
+  cv2.imshow('gray', gray)
+  cv2.imshow('bgr ', bgr)
+  
+  if cv2.waitKey(0) & 0xff == 27:  #escape
+    cv2.destroyAllWindows()
 threshold('img2.jpg')
