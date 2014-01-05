a3ff4ef06273efd0c2de97917b4383c9b12adef8
treeherder <b.d.reddybest@gmail.com>
2014-01-05 00:31:35 -0800

    OOP

--- src/cam.py
+++ src/cam.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 c = cv2.VideoCapture(0)
 
-while(1):
+def main():
   ret,frame = c.read()
   cv2.imshow('frames',frame)
-  if cv2.waitKey(5)==27:   #press esc to exit  
-    break
-cv2.destroyAllWindows()
+while True:
+   main()
+   if cv2.waitKey(5)==27:   #press esc to exit  
+     break
+     cv2.destroyAllWindows()
