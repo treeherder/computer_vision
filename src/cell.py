764e26860d2d6d32b33e16cf77eda9006c52a352
treeherder <b.d.reddybest@gmail.com>
2014-01-10 06:08:26 -0800

    bounding rectangles are working, but too much noise inside the perimeter

--- src/cell.py
+++ src/cell.py
@@ -21,12 +21,15 @@ def mask(img):
   contours, hierarchy = cv2.findContours(thresh,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
   for cnt in contours:
     area = cv2.contourArea(cnt)
-    if area >1000 and area < 2000:
+    if area > 100 and area < 2000:
       peri = cv2.arcLength(cnt, True)
       approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
       if area > max_area and len(approx)==4:
       	biggest = approx
       	max_area = area
+      x,y,w,h = cv2.boundingRect(cnt)
+      cv2.rectangle(blk, (x,y),(x+w,y+h),(0,255,0),2)
+
       cv2.drawContours(res,[approx],0,(20,130,50),-1)
   cv2.imshow('contour-highlighted image.jpg', res )
   cv2.imshow('edges', blk)
