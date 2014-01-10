3680021b3a641b81f8591eedd956e1fee2866545
treeherder <b.d.reddybest@gmail.com>
2014-01-10 06:46:59 -0800

    struggling with cell

--- src/cell.py
+++ src/cell.py
@@ -21,14 +21,14 @@ def mask(img):
   contours, hierarchy = cv2.findContours(thresh,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
   for cnt in contours:
     area = cv2.contourArea(cnt)
-    if area > 100 and area < 2000:
+    if area > 100 and area < 2000:  #an arbitrary limit to stop the whole frame from being bound
       peri = cv2.arcLength(cnt, True)
-      approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+      approx = cv2.approxPolyDP(cnt, peri, True)
       if area > max_area and len(approx)==4:
       	biggest = approx
       	max_area = area
       x,y,w,h = cv2.boundingRect(cnt)
-      cv2.rectangle(blk, (x,y),(x+w,y+h),(0,255,0),2)
+      cv2.rectangle(res, (x,y),(x+w,y+h),(0,255,0),2)
 
       cv2.drawContours(res,[approx],0,(20,130,50),-1)
   cv2.imshow('contour-highlighted image.jpg', res )
