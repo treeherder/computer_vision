09c72128cd319aef9b3dff04ac1f06ac37c306d3
treeherder <b.d.reddybest@gmail.com>
2014-01-10 05:24:23 -0800

    thresholding lower bounds fixed
    need to fill square with hull ?

--- src/thresholding.py
+++ src/thresholding.py
@@ -13,13 +13,17 @@ def threshold(z):
   (hue, sat, val)  = cv2.split(hsv)
   hf, hue  = cv2.threshold(hue, 255, 255, cv2.THRESH_BINARY)   #0,20
   sf, sat  = cv2.threshold(sat, 255, 255, cv2.THRESH_BINARY)   #0,20
-  vf, val  = cv2.threshold(val, 45, 255, cv2.THRESH_BINARY)  #30,45
+  vf, val  = cv2.threshold(val, 10, 255, cv2.THRESH_BINARY)  #30,45
   mrgd = cv2.merge([hue, sat, val])    #threshold for just the black square
   bgr = cv2.cvtColor(mrgd, cv2.COLOR_HSV2BGR)
   gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
   mask = ors.cntr(gray)
   edges = cv2.Canny(mask,100,200)
   cv2.imshow('mask',edges)
+  cv2.imshow('hue',hue)
+  cv2.imshow('sat',sat)
+  cv2.imshow('val',val)
+
   cv2.imwrite("../images/t-{0}".format(z), edges)
   if cv2.waitKey(0) & 0xff == 27:  #escape
     cv2.destroyAllWindows()
