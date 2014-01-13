2a5f602ae215e7e08d39853a2b115a0083fc5390
treeherder <b.d.reddybest@gmail.com>
2014-01-12 22:14:51 -0800

    detecting the proper hierarchy to get  a fix on the bottom edge of the cell

--- src/cell.py
+++ src/cell.py
@@ -14,24 +14,27 @@ def mask(img):
   biggest =None
   max_area = 0
   grey = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-  blk = cv2.bitwise_not(grey)
+  #blk = cv2.bitwise_not(grey)
   kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-  res = cv2.morphologyEx(blk,cv2.MORPH_OPEN,kernel)
-  ret,thresh = cv2.threshold(blk,127,255,0)
+  res = cv2.morphologyEx(grey,cv2.MORPH_OPEN,kernel)
+  ret,thresh = cv2.threshold(grey,127,255,0)
   contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
   dest = np.zeros(thresh.shape, np.uint8)
   print contours[1:]
   print len(contours)
   print hierarchy
-  for cnt in contours[1:]:
-    if cnt.any() in hierarchy[0][1:]:
-      rect  = cv2.minAreaRect(cnt)
-      points = cv2.cv.BoxPoints(rect)
-      points  = np.int0(np.around(points))
-      cv2.drawContours(dest, [cnt],0,(0,255,0),2)
-      cv2.polylines(dest, [points], True,( 255,255,255), 2 )
-    cv2.imshow('contour-highlighted image.jpg', dest)
+  for cnt in contours[4:21]:
+    rect  = cv2.minAreaRect(cnt)
+    points = cv2.cv.BoxPoints(rect)
+    points  = np.int0(np.around(points))
+    #cv2.drawContours(dest, [cnt],0,(0,255,0),2)
+    cv2.polylines(dest, [points], True,( 255,255,255), 2 )
+    #cv2.fillPoly(dest, [cnt], (255,255,255), 4)
+
+    x = cv2.cvtColor(dest,cv2.COLOR_GRAY2RGB)
+    cv2.imshow('contour-highlighted image.jpg', x)
     cv2.imwrite("../images/bound.jpg", dest)
+
     cv2.imshow('edges', thresh)
 if __name__ == '__main__':
   mask(frame)	
