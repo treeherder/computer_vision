a22374cfe84c7a5f993d115e868de40684147b55
treeherder <b.d.reddybest@gmail.com>
2014-02-11 01:36:34 -0800

    thresholding on wire works

--- src/python/wire_thresholding.py
+++ src/python/wire_thresholding.py
@@ -12,8 +12,8 @@ def threshold(z):
   hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
   (hue, sat, val)  = cv2.split(hsv)
   hf, hue  = cv2.threshold(hue, 255, 255, cv2.THRESH_BINARY)   #0,20
-  sf, sat  = cv2.threshold(sat, 255, 255, cv2.THRESH_BINARY)   #0,20
-  vf, val  = cv2.threshold(val, 10, 255, cv2.THRESH_BINARY)  #30,45
+  sf, sat  = cv2.threshold(sat, 100, 155, cv2.THRESH_BINARY)   #0,20
+  vf, val  = cv2.threshold(val, 0, 255, cv2.THRESH_BINARY)  #30,45
   mrgd = cv2.merge([hue, sat, val])    #threshold for just the black square
   bgr = cv2.cvtColor(mrgd, cv2.COLOR_HSV2BGR)
   gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
