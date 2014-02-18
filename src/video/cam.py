c4ad7d24ce2066029efb29597ac13db6afdb5ccd
treeherder <b.d.reddybest@gmail.com>
2014-02-18 04:29:58 -0800

    anxious to check this on reasonable data

--- src/video/cam.py
+++ src/video/cam.py
@@ -16,7 +16,30 @@ def main():
   blur = cv2.GaussianBlur(gray, (5,5), 0)
   mask = ors.cntr(gray)
   edges = cv2.Canny(mask,100,200)
-  cv2.imshow('mask',edges)
+  #cv2.imshow('mask',edges)
+  biggest =None
+  max_area = 0
+  #blk = cv2.bitwise_not(grey)
+  kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+  res = cv2.morphologyEx(edges,cv2.MORPH_OPEN,kernel)
+  ret,thresh = cv2.threshold(edges,127,255,0)
+  contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+  dest = np.zeros(thresh.shape, np.uint8)
+  print contours[::1]
+  print len(contours)
+  print hierarchy
+  for cnt in contours[::1]:
+    rect  = cv2.minAreaRect(cnt)
+    points = cv2.cv.BoxPoints(rect)
+    points  = np.int0(np.around(points))
+    #cv2.drawContours(dest, [cnt],0,(0,255,0),2)
+    #cv2.polylines(dest, [points], True,( 255,255,255), 2 )
+    cv2.fillPoly(edges, [cnt], (100,20,90), 4)
+    cv2.fillPoly(dest, [cnt], (255,255,255), 4)
+
+    x = cv2.cvtColor(dest,cv2.COLOR_GRAY2RGB)
+    cv2.imshow('contour-highlighted image.jpg', x)
+ 
 
 while 1:
   main()
